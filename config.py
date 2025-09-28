ebb723e5258a82c5914f69ca6c872d963eaff108
VoidXTora <priyanshuprasad853@gmail.com>
2025-09-28 20:48:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = os.getenv("API_HASH", "69daa0835439c4211f34c2e9ad0acb5c")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "6809164496:AAGmTBe5dCMBFH4HG6zzON87MPKsOSzo6d4")
 
 # Work directory for temp files
-WORKDIR = "downloads"
+WORKDIR = "/downloads"
 
 # MongoDB
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Haruto179:Haruto179@cluster179.hy2mawo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster179")
@@ -15,6 +15,9 @@ DB_NAME = "Haruto179"
 # Dump channel (optional)
 DUMP_CHANNEL_ID = int(os.getenv("DUMP_CHANNEL_ID", "0"))
 LOG_CHANNEL = int(os.getenv("DUMP_CHANNEL_ID", "0"))
+
+# Optional bot uptime
+BOT_UPTIME = os.environ.get("BOT_UPTIME", None)
 
 
 ADMIN = "6617544956"
